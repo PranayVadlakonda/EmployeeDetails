b42e16a05bfe242951a2678ec1bafa374be659eb
pranay <pranayanand61@gmail.com>
2023-03-19 22:35:57 +0530

    changed the source and target jdk version to 11

--- src/main/java/com/niit/jap/Main.java
+++ src/main/java/com/niit/jap/Main.java
@@ -2,6 +2,8 @@ package com.niit.jap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! pransy");
     }
 }
+
+
